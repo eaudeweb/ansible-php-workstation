411322446fec525ba8dad74ecaeb325fc2e1094d
Cristian Romanescu <cristian@romanescu.ro>
2024-05-29 20:31:50 +0300

    Bootstrap installation

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 apt install -y ansible git
+
+if [ ! -d ansible-php-workstation ]; then
+  git clone https://github.com/eaudeweb/ansible-php-workstation.git ansible-deskop
+fi
+
+cd ansible-deskop
+ansible-playbook playbook.yml
