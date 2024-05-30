9b2c7fec29499b41a3d721b0346ff1d839a2eee8
Cristian Romanescu <cristian@romanescu.ro>
2024-05-30 22:43:48 +0300

    Explicit sudo for base packages

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apt install -y ansible git
+sudo apt install -y ansible git
 
 if [ ! -d ansible-php-workstation ]; then
   git clone https://github.com/eaudeweb/ansible-php-workstation.git ansible-deskop
